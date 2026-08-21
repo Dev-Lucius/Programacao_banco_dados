b802d41e8099160d6e6bc82fea41dd951eb93042
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-08-21 17:04:32 -0300

    Refactor nome_jogo function for clarity

--- exercicios/ex05.sql
+++ exercicios/ex05.sql
@@ -2,16 +2,26 @@
 Crie uma função nome_jogo(jogo_id) que retorne uma string no formato:
 - SAO PAULO DE RG x RIO GRANDE
 */
-CREATE OR REPLACE FUNCTION nome_jogo(var_jogo_id integer) RETURNS text AS
-$$
+CREATE OR REPLACE FUNCTION nome_jogo(var_jogo_id integer) 
+RETURNS text 
+LANGUAGE plpgsql
+AS $$
 DECLARE
     resultado_casa text;
     resultado_visitante text;
 BEGIN
-    SELECT nome FROM equipe JOIN jogo ON equipe.id = jogo.equipe_casa_id WHERE jogo.id = var_jogo_id INTO resultado_casa;
+    SELECT nome 
+    INTO resultado_casa
+    FROM equipe 
+    JOIN jogo ON equipe.id = jogo.equipe_casa_id 
+    WHERE jogo.id = var_jogo_id;
     
-    SELECT nome FROM equipe JOIN jogo ON equipe.id = jogo.equipe_visitante_id WHERE jogo.id = var_jogo_id INTO resultado_visitante;
+    SELECT nome 
+    INTO resultado_visitante
+    FROM equipe 
+    JOIN jogo ON equipe.id = jogo.equipe_visitante_id 
+    WHERE jogo.id = var_jogo_id;
     
     RETURN resultado_casa || ' x ' || resultado_visitante;
 END;
-$$ LANGUAGE 'plpgsql';
+$$;
