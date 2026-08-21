9b7bbc39ec8fab0443a4adf48bb35832d81ed22f
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-08-21 17:03:24 -0300

    Refactor nome_equipe function for clarity

--- exercicios/ex04.sql
+++ exercicios/ex04.sql
@@ -1,10 +1,16 @@
 -- 4. Crie uma função nome_equipe(equipe_id) que retorne o nome da equipe.
-CREATE OR REPLACE FUNCTION nome_equipe(var_equipe_id INTEGER) RETURNS TEXT AS
-$$
+CREATE OR REPLACE FUNCTION nome_equipe(var_equipe_id INTEGER) 
+RETURNS TEXT 
+LANGUAGE plpgsql
+AS $$
 DECLARE 
     resultado TEXT;
 BEGIN
-    SELECT nome FROM equipe WHERE id = var_equipe_id INTO resultado;
+    SELECT nome 
+    INTO resultado        
+    FROM equipe 
+    WHERE id = var_equipe_id;
+    
     RETURN resultado;
-END
-$$ LANGUAGE 'plpgsql';
+END;
+$$;
